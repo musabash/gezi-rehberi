8cd51ad57c774444fa4b7adbb2dfbb7b4e13285a
Musa Bas <basmusa@icloud.com>
2024-07-07 21:42:27 +0100

    feat: add get-data handler

--- functions/get-data.js
+++ functions/get-data.js
@@ -1,24 +1,18 @@
 const axios = require('axios');
 // require('dotenv').config();
 
-// exports.handler = async (event, context) => {
-//     try {
-//         const response = await axios.get(`https://sheets.googleapis.com/v4/spreadsheets/${Netlify.env.SPREADSHEET_ID}/values/${Netlify.env.RANGE}?key=${Netlify.env.API_KEY}`);
-//         return {
-//             statusCode: 200,
-//             body: JSON.stringify(response.data),
-//         };
-//     } catch (error) {
-//         console.error('Error fetching data:', error);
-//         return {
-//             statusCode: 500,
-//             body: 'Failed to fetch data',
-//         };
-//     }
-// };
-
-export default async (req, context) => {
-  return new Response("Hello, world!");
+exports.handler = async (event, context) => {
+    try {
+        const response = await axios.get(`https://sheets.googleapis.com/v4/spreadsheets/${Netlify.env.SPREADSHEET_ID}/values/${Netlify.env.RANGE}?key=${Netlify.env.API_KEY}`);
+        return {
+            statusCode: 200,
+            body: JSON.stringify(response.data),
+        };
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        return {
+            statusCode: 500,
+            body: 'Failed to fetch data',
+        };
+    }
 };
-
-
