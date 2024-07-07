dec505776cfe8eb2314d1fcc244141b9f6e1b224
Musa Bas <basmusa@icloud.com>
2024-07-07 20:10:02 +0100

    fix: webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,22 @@
+// const path = require('path');
+
+// module.exports = {
+//   entry: 'index.js',
+//   output: {
+//     filename: 'bundle.js',
+//     path: path.resolve(__dirname, 'dist')
+//   },
+//   mode: 'production'
+// };
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  entry: './index.js',
+  entry: 'index.js',
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, './dist'),
+    filename: 'index_bundle.js',
   },
-  mode: 'production'
+  plugins: [new HtmlWebpackPlugin()],
 };
