be6321a0538fd84121156e47e81c97ff11337e02
Musa Bas <basmusa@icloud.com>
2024-07-07 21:39:19 +0100

    fix: index.html

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,6 @@ module.exports = {
   plugins: [new HtmlWebpackPlugin({
       template: './src/index.html',
       inject: true,
-      filename: './src/index.html'
+      filename: 'index.html'
     })],
 };
